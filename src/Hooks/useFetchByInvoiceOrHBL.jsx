67a6bef076bfc79bd7b18227003283f35576eb50
yochirolee <yleecruz@gmail.com>
2023-04-27 17:31:57 -0400

    reat-query fix

--- src/Hooks/useFetchByInvoiceOrHBL.jsx
+++ src/Hooks/useFetchByInvoiceOrHBL.jsx
@@ -1,24 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 
-const getInvoiceById = async (id) => {
-	const res = await fetch("https://caribe-cargo-api.vercel.app/api/invoices/" + id);
+const getProductData = async (id) => {
+	if (id && id.length > 7) {
+		const res = await fetch("https://caribe-cargo-api.vercel.app/api/products/" + id);
+		return res.json();
+	} else {
+		const res = await fetch("https://caribe-cargo-api.vercel.app/api/invoices/" + id);
 
-	return res.json();
-};
-
-const getProductByHBL = async (id) => {
-	const res = await fetch("https://caribe-cargo-api.vercel.app/api/products/" + id);
-	return res.json();
+		return res.json();
+	}
 };
 
 export const useFetchByInvoiceOrHBL = (id) => {
-	if (id && id.length > 7) {
-		return useQuery(["fetchProductByHBL", id], () => getProductByHBL(id), {
-			enabled: Boolean(id),
-		});
-	} else {
-		return useQuery(["fetchInvoiceById", id], () => getInvoiceById(id), {
-			enabled: Boolean(id),
-		});
-	}
+	return useQuery(["fetchProductByHBL", id], () => getProductData(id), {
+		enabled: Boolean(id),
+	});
 };
