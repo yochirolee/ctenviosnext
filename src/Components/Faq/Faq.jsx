3a58f4ff9fc4b3d120e28ed0c4e8ed6537ed33d2
yochirolee <yleecruz@gmail.com>
2023-04-27 13:49:44 -0400

    added faq section ds

--- src/Components/Faq/Faq.jsx
+++ src/Components/Faq/Faq.jsx
@@ -13,7 +13,7 @@ export const Faq = () => {
 					<h3 class="text-sm font-semibold leading-7 text-slate-400">General</h3>
 					<dl class="mt-2 divide-y divide-slate-100">
 						<details class="group py-4 ">
-							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [&amp;::-webkit-details-marker]:hidden">
+							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [`&amp;`::-webkit-details-marker]:hidden">
 								Que tiempo demora un Envio?
 								<svg
 									class="ml-4 mt-0.5 h-6 w-6 flex-none stroke-slate-700 group-open:stroke-blue-500"
@@ -39,7 +39,7 @@ export const Faq = () => {
 							</div>
 						</details>
 						<details class="group py-4 ">
-							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [&amp;::-webkit-details-marker]:hidden">
+							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [`&amp;`::-webkit-details-marker]:hidden">
 								Donde puedo conocer el estado de un Envio?
 								<svg
 									class="ml-4 mt-0.5 h-6 w-6 flex-none stroke-slate-700 group-open:stroke-blue-500"
@@ -94,7 +94,7 @@ export const Faq = () => {
 					<h3 class="text-sm font-semibold leading-7 text-slate-400">Aduana Cuba</h3>
 					<dl class="mt-2 divide-y divide-slate-100">
 						<details class="group py-4 ">
-							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [&amp;::-webkit-details-marker]:hidden">
+							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [`&amp;`::-webkit-details-marker]:hidden">
 								Are Figma, Sketch, or Adobe XD files included?
 								<svg
 									class="ml-4 mt-0.5 h-6 w-6 flex-none stroke-slate-700 group-open:stroke-blue-500"
@@ -119,103 +119,6 @@ export const Faq = () => {
 										much time creating new examples in code which is where we believe we can provide
 										the most value.
 									</p>
-								</div>
-							</div>
-						</details>
-						<details class="group py-4 ">
-							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [&amp;::-webkit-details-marker]:hidden">
-								What JS framework is used?
-								<svg
-									class="ml-4 mt-0.5 h-6 w-6 flex-none stroke-slate-700 group-open:stroke-blue-500"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								>
-									<path d="M18 12H6"></path>
-									<path class="group-open:hidden" d="M12 6v12"></path>
-								</svg>
-							</summary>
-							<div class="pb-6 pt-6">
-								<div class="prose prose-slate max-w-none prose-a:font-semibold prose-a:text-blue-600 hover:prose-a:text-blue-500">
-									<p>
-										All of the components in Tailwind UI are provided in three formats: React, Vue,
-										and vanilla HTML.
-									</p>
-									<p>
-										The React and Vue examples are fully functional out-of-the-box, and are powered
-										by <a href="https://headlessui.dev">Headless UI</a> — a library of unstyled
-										components we designed to integrate perfectly with Tailwind CSS. Headless UI
-										supports both React and Vue 3, but is not compatible with Vue 2.
-									</p>
-									<p>
-										The vanilla HTML examples <strong>do not include any JavaScript</strong> and are
-										designed for people who prefer to write any necessary JavaScript themselves.
-									</p>
-									<p>
-										Most of the components do not rely on JS at all, but for the ones that do
-										(dropdowns, modals, etc.) we've provided some simple comments in the HTML to
-										explain things like what classes you need to use for different states (like a
-										toggle switch being on or off), or what classes we recommend for transitioning
-										elements on to or off of the screen (like a modal opening).
-									</p>
-									<p>
-										To get a better idea of how this looks in practice,{" "}
-										<a href="/documentation#integrating-with-javascript-frameworks">
-											check out our documentation
-										</a>
-										.
-									</p>
-								</div>
-							</div>
-						</details>
-						<details class="group py-4 ">
-							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [&amp;::-webkit-details-marker]:hidden">
-								What version of Tailwind CSS is used?
-								<svg
-									class="ml-4 mt-0.5 h-6 w-6 flex-none stroke-slate-700 group-open:stroke-blue-500"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								>
-									<path d="M18 12H6"></path>
-									<path class="group-open:hidden" d="M12 6v12"></path>
-								</svg>
-							</summary>
-							<div class="pb-6 pt-6">
-								<div class="prose prose-slate max-w-none prose-a:font-semibold prose-a:text-blue-600 hover:prose-a:text-blue-500">
-									<p>
-										Everything in Tailwind UI is designed and developed for the latest version of
-										Tailwind CSS, which is currently Tailwind CSS v3.3.
-									</p>
-								</div>
-							</div>
-						</details>
-						<details class="group py-4 ">
-							<summary class="flex w-full cursor-pointer select-none justify-between text-left text-base font-semibold leading-7 text-slate-900 group-open:text-blue-600 [&amp;::-webkit-details-marker]:hidden">
-								What browsers are supported?
-								<svg
-									class="ml-4 mt-0.5 h-6 w-6 flex-none stroke-slate-700 group-open:stroke-blue-500"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								>
-									<path d="M18 12H6"></path>
-									<path class="group-open:hidden" d="M12 6v12"></path>
-								</svg>
-							</summary>
-							<div class="pb-6 pt-6">
-								<div class="prose prose-slate max-w-none prose-a:font-semibold prose-a:text-blue-600 hover:prose-a:text-blue-500">
-									<p>
-										The components in Tailwind UI are designed to work in the latest, stable
-										releases of all major browsers, including Chrome, Firefox, Safari, and Edge.
-									</p>
-									<p>We don't support Internet Explorer 11.</p>
 								</div>
 							</div>
 						</details>
