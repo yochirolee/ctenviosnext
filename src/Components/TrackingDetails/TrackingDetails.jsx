1d02ccc63c0ed04a98a45bbf48a47fb24fc7db31
yochirolee <yleecruz@gmail.com>
2023-05-02 14:22:21 -0400

    fixed showing when not result

--- src/Components/TrackingDetails/TrackingDetails.jsx
+++ src/Components/TrackingDetails/TrackingDetails.jsx
@@ -2,9 +2,10 @@ import { React } from "react";
 import { TrackingCard } from "../Cards/TrackingCard";
 
 export const TrackingDetails = ({ result }) => {
-	if (!result) return;
+	console.log(result, "RESUTL");
+	if (!result || result.length == 0) return;
 	return (
-		<div className="flex  flex-col gap-4 max-w-2xl mx-auto ">
+		<div className="flex  flex-col  gap-4 max-w-2xl mx-auto ">
 			{result?.Products ? (
 				result?.Products?.map((product) => <TrackingCard key={product?.HBL} product={product} />)
 			) : (
