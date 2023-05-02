31ef73455c8e31834c10f6b4de51c0c858d55f4f
yochirolee <yleecruz@gmail.com>
2023-05-02 14:15:00 -0400

    shopping card add fixxed missing key

--- src/Components/Products/ProductsList.jsx
+++ src/Components/Products/ProductsList.jsx
@@ -83,8 +83,8 @@ export default function ProductsList() {
 
 				<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 					{products.map((product) => (
-						<div>
-							<a key={product.id} href={product.href} className="group">
+						<div key={product.id}>
+							<a href={product.href} className="group">
 								<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
 									<img
 										src={product.imageSrc}
