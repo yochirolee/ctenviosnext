8dac1ee25b23d2a3149ca1abf9fe50dce891143b
yochirolee <yleecruz@gmail.com>
2023-05-24 11:18:58 -0400

    google tag added 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,7 +43,6 @@ export default function RootLayout({ children }) {
 						<QueryProvider>
 							<AuthProvider>
 								<NavBar />
-
 								{children}
 								<ShoppingCart />
 								<Footer />
