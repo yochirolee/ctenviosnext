2b5f6499bbe49739a11444653a922f192e3ca834
yochirolee <yleecruz@gmail.com>
2024-01-10 12:14:22 -0500

    stats1

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -8,7 +8,7 @@ export const Stats = () => {
 	return (
 		<div className="bg-white  py-10 sm:py-32">
 			<div className="mx-auto  max-w-7xl px-6 lg:px-8">
-				<dl className=" grid grid-cols-1 divide-x  gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+				<dl className=" grid grid-cols-1 lg:divide-x  gap-x-8 gap-y-16 text-center lg:grid-cols-3">
 					{stats.map((stat) => (
 						<div key={stat.id} className="mx-auto px-4 flex max-w-xs flex-col gap-y-4">
 							<div className="text-base leading-7 text-gray-600">{stat.name}</div>
