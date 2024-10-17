bade9643138015cdbe26f3018b4585c9a609eba9
yosho <yleecruz@gmail.com>
2024-10-17 15:50:51 -0400

    Update page.js

--- src/app/tracking/page.js
+++ src/app/tracking/page.js
@@ -2,7 +2,7 @@
 import { HeroTracking } from "@/Components/Hero/HeroTracking";
 import { TrackingContent } from "@/Components/Content/TrackingContent";
 import { React } from "react";
-import { Stats } from "@/Components/Stats//Stats";
+import { Stats } from "@/Components/Stats/Stats";
 
 const Tracking = () => {
 	return (
