abf527f1b67060f2975baf956d267d379638715b
yosho <yleecruz@gmail.com>
2024-12-27 14:51:06 -0500

    updated google analytics

--- src/Components/Analytics/GoogleAnalytics.jsx
+++ src/Components/Analytics/GoogleAnalytics.jsx
@@ -5,16 +5,17 @@ export default function GoogleAnalytics() {
 	return (
 		<>
 			<Script
-				src="https://www.googletagmanager.com/gtag/js?id=AW-11165948511"
+				src="https://www.googletagmanager.com/gtag/js?id=G-DMGE29VG1R"
 				strategy="afterInteractive"
 				defer
 			/>
 			<Script id="google-analytics" strategy="afterInteractive" defer>
 				{`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-11165948511');
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DMGE29VG1R');
         `}
 			</Script>
 		</>
