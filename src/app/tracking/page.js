70d6ddc5f67d5b96c120d9635b8fb4e1d351a109
yosho <yleecruz@gmail.com>
2024-12-27 12:34:36 -0500

    updated search2

--- src/app/tracking/page.js
+++ src/app/tracking/page.js
@@ -1,15 +1,17 @@
 "use client";
 import { HeroTracking } from "@/Components/Hero/HeroTracking";
 import { TrackingContent } from "@/Components/Content/TrackingContent";
-import { React } from "react";
+import { React, Suspense } from "react";
 import { Stats } from "@/Components/Stats/Stats";
 
 const Tracking = () => {
 	return (
 		<>
-			<HeroTracking />
-			<Stats />
-			<TrackingContent />
+			<Suspense fallback={<div>Loading...</div>}>
+				<HeroTracking />
+				<Stats />
+				<TrackingContent />
+			</Suspense>
 		</>
 	);
 };
