7813fdcef7b6348d2c204f252a54403039696134
yosho <yleecruz@gmail.com>
2025-04-08 11:29:11 -0400

    pr ui 6

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,7 @@ import { SocialMedia } from "@/Components/SocialMedia/SocialMedia";
 import { Stats } from "@/Components/Stats/Stats";
 import TrackingSection from "./sections/tracking-section";
 import Faq from "@/Components/Faq/Faq";
-import dynamic from "next/dynamic";
 import PriceCards from "@/Components/Cards/PricesCards";
-
-const FeaturedProducts = dynamic(() => import("./sections/feaured-products"), {
-	loading: () => <p>Loading featured products...</p>,
-	ssr: true,
-});
 
 export default function Home() {
 	return (
