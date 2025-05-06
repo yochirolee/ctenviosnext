a443affd2935be6ed6675a6a03594951916d3811
yosho <yleecruz@gmail.com>
2025-05-06 15:59:19 -0400

    pr css improvement 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -111,12 +111,6 @@ export const viewport = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="es">
-			<head>
-				<link rel="preconnect" href="https://www.google-analytics.com" />
-				<link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="anonymous" />
-				<link rel="dns-prefetch" href="https://www.google-analytics.com" />
-				<link rel="dns-prefetch" href="https://www.googletagmanager.com" />
-			</head>
 			<body className={inter.className}>
 				<div className="relative isolate pt-4 max-w-7xl mx-auto sm:px-6 lg:pt-14 lg:px-8">
 					<header>
@@ -127,7 +121,7 @@ export default function RootLayout({ children }) {
 
 				<Footer />
 			</body>
-			<GoogleAnalytics gaId="G-E53C6QQ7Z8" />
+			<GoogleAnalytics gaId="G-E53C6QQ7Z8" strategy="afterInteractive" />
 		</html>
 	);
 }
